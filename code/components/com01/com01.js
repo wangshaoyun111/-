1a174e049ed3b8283d657dc3f9b820d39c82f794
wsy <18611031643.163.com>
2021-05-25 16:28:09 +0800

    小程序对数组对象的监听

--- code/components/com01/com01.js
+++ code/components/com01/com01.js
@@ -15,6 +15,11 @@ Component({
    */
   data: {
     age:18,
+    obj:{
+      num:0,
+      max:1
+    },
+    arr:[1,2,3,4]
   },
 
   observers:{
@@ -26,6 +31,17 @@ Component({
     },
     'age,mynum':function(newAge,newNum){
       console.log(newAge,newNum);
+    },
+    // 'obj.num': function(newNum){
+    //   console.log(newNum);
+    // },
+    // 'arr[1]':function(newArr){
+    //   console.log(newArr);
+    // },
+    // 对 数组和对象 进行监听，对 对象进行监听通过obj.num方式进行监听
+    // 对 数组进行监听通过索引方式进行监听
+    'obj.num,arr[1]':function(newNum,newArr){
+      console.log(newNum,newArr);
     }
   },
   /**
@@ -33,10 +49,15 @@ Component({
    */
   methods: {
     changeData(){
+      // 如果需要对数组或者对象中某一项进行修改
       this.setData({
-        age:19,
-        mynum:20
+        'obj.num':10, // 对象修改通过点方式
+        'arr[1]':20 // 数组是索引方式
       })
+      // this.setData({
+      //   age:19,
+      //   mynum:20
+      // })
     }
   }
 })
