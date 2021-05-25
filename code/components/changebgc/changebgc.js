384bc945e78a7703fb51f5e344cd54085e728177
wsy <18611031643.163.com>
2021-05-25 17:32:18 +0800

    小程序监听器练习布局

---
+++ code/components/changebgc/changebgc.js
@@ -0,0 +1,28 @@
+// components/changebgc/changebgc.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    rgb:{
+      r:0,
+      g:0,
+      b:0
+    },
+    fullColor:'0,0,0'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
