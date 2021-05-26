e7e611d9c9c235ddb8b4aae4fcb934e2a9faff6b
wsy <18611031643.163.com>
2021-05-26 11:08:27 +0800

    小程序纯属就字段，和组件钩子函数

--- code/components/changebgc/changebgc.js
+++ code/components/changebgc/changebgc.js
@@ -1,5 +1,8 @@
 // components/changebgc/changebgc.js
 Component({
+  options:{
+    pureDataPattern:/^_/
+  },
   /**
    * 组件的属性列表
    */
@@ -11,12 +14,30 @@ Component({
    * 组件的初始数据
    */
   data: {
-    rgb:{
+    age:11,
+    _rgb:{
       r:0,
       g:0,
       b:0
     },
     fullColor:'0,0,0'
+  },
+  // 官方推荐在lifetimes中使用钩子函数
+  lifetimes:{
+    // setData 不生效
+    created(){},
+    attached(){
+      this.setData({
+        // age:111
+      })
+    },
+  },
+  // 组件所在页面，父组件的状态
+  pageLifetimes:{
+    // 父组件打开成功
+    show(){},
+    // 父组件隐藏
+    hide(){}
   },
 
   observers:{
@@ -27,7 +48,7 @@ Component({
     // }
     // 如果需要对对象中每一项进行监听,可以使用通配符 ** 监听所有对象的字段
     // 监听器函数参数为 对象 ,对象有更新以后所有的值
-    'rgb.**':function(obj){
+    '_rgb.**':function(obj){
         this.setData({
           fullColor:`${obj.r},${obj.g},${obj.b}`
         })
@@ -40,17 +61,17 @@ Component({
   methods: {
     changeR () {
       this.setData({
-        'rgb.r': this.data.rgb.r >= 255 ? '255' : this.data.rgb.r + 5
+        '_rgb.r': this.data._rgb.r >= 255 ? '255' : this.data._rgb.r + 5
       })
     },
     changeG () {
       this.setData({
-        'rgb.g': this.data.rgb.g >= 255 ? '255' : this.data.rgb.g + 5
+        '_rgb.g': this.data._rgb.g >= 255 ? '255' : this.data._rgb.g + 5
       })
     },
     changeB () {
       this.setData({
-        'rgb.b': this.data.rgb.b >= 255 ? '255' : this.data.rgb.b + 5
+        '_rgb.b': this.data._rgb.b >= 255 ? '255' : this.data._rgb.b + 5
       })
     }
   }
