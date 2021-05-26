3dba9a079f887242424cc00a0c6108c851075d47
wsy <18611031643.163.com>
2021-05-26 16:17:57 +0800

    小程序事件绑定方式，子组件向父组件传值

---
+++ code/components/com03/com03.js
@@ -0,0 +1,26 @@
+// components/com03/com03.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    name:'Tom'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    handle(){
+      // 3.通过triggerEvent 方法接收父组件得自定义事件，第二个参数为传递给父组件的数据
+      this.triggerEvent('myevent',this.data.name)
+    }
+  }
+})
