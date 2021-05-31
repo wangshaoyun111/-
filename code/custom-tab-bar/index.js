26d5365765a3ef99c99aa5135a0d739c446ab4e3
wsy <18611031643.163.com>
2021-05-31 16:52:54 +0800

    小程序自定义tarbar实现编程序导航跳转

--- code/custom-tab-bar/index.js
+++ code/custom-tab-bar/index.js
@@ -28,20 +28,20 @@ Component({
     active:0,
     list:[
       {
-        pagePath: "pages/home/home",
+        pagePath: "/pages/home/home",
         text: "首页",
         iconPath: "/images/tabs/home.png",
         selectedIconPath: "/images/tabs/home-active.png"
       },
       {
-        pagePath: "pages/message/message",
+        pagePath: "/pages/message/message",
         text: "消息",
         iconPath: "/images/tabs/message.png",
         selectedIconPath: "/images/tabs/message-active.png",
         info:3
       },
       {
-        pagePath: "pages/contact/contact",
+        pagePath: "/pages/contact/contact",
         text: "联系我们",
         iconPath: "/images/tabs/contact.png",
         selectedIconPath: "/images/tabs/contact-active.png"
@@ -63,6 +63,9 @@ Component({
     onChange(event) {
       // event.detail 的值为当前选中项的索引
       this.setData({ active: event.detail });
+      wx.switchTab({
+        url: this.data.list[event.detail].pagePath,
+      })
     }
   }
 })
